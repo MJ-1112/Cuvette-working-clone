32f8e27300cdc5934d02e387f64212c6ea2ed8a3
Mahik <m22327803@gmail.com>
2025-03-16 06:41:55 +0530

    added

---
+++ views/index.js
@@ -0,0 +1,13 @@
+function clicker(){
+    window.location.href = "main.html";
+}
+function clickboo(){
+    window.location.href = "Full-Time.html";
+}
+function clickbood(){
+    window.location.href = "Other.html";
+}
+function clickboode(){
+    window.location.href = "Applied.html";
+}
+
